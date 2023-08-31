4c2456f37a5cd23af4a4c48141404f4d5fd6296e
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-08-31 22:22:44 +0530

    Add files via upload

---
+++ Chapter1PS.py
@@ -0,0 +1,10 @@
+# Q:1 Write a program to print Twinkle twinkle litle star poem in python.
+print("Twinkle, twinkle, little star,\n How I wonder what you are!\n Up above the world so high, \nLike a diamond in the sky.")
+
+# Q:2 Use REPL and print the table of 5 using it.
+
+# Q:3 Install an external module and use it to perform an operation of your interest.
+ 
+# Q:4 Write a python program to print the contents of a directory using OS module. Search online for the function which does that.
+
+# Q:5 Lable the program written in python 4 with comments.
